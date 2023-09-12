3cbe89f0bf66358586fc1b8eff71527d64fc7c42
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-09-12 21:52:28 +0530

    ADD: Tailwind css and hero section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header';
+import Home from "./pages/home";
 // import Home from './components/home/Home';
 // import About from './components/about/About';
 // import Skills from './components/skills/Skills';
@@ -9,7 +8,7 @@ import Header from './components/header/header';
 function App() {
   return (
     <>
-      <Header />
+      <Home/>
       {/* <main className="main">
         <Home />
         <About />
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/heroSection/hero'
+import Header from '../components/header/header'
+
+export default function home() {
+  return (
+    <React.Fragment>
+        <Header />
+        <Hero />
+    </React.Fragment>
+  )
+}
