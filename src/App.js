3bcc4e15cd4e473b08576f46e0943f396a00ff22
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2023-09-12 15:59:29 +0530

    Dev: navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+// import Home from './components/home/Home';
+// import About from './components/about/About';
+// import Skills from './components/skills/Skills';
+// import Services from './components/services/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {/* <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+      </main> */}
+    </>
   );
 }
 
