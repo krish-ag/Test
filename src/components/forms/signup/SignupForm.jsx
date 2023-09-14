f6a2d6350735151be7ddd3b2173300f72d15c1aa
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-09-14 22:02:33 +0530

    FIX: signup form

--- src/components/forms/signup/SignupForm.jsx
+++ src/components/forms/signup/SignupForm.jsx
@@ -33,7 +33,7 @@ export default function SignupForm() {
               </h1>
               {isPassenger && (<form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email/username</label>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"/>
                   </div>
                   <div>
