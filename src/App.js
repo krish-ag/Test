13cade79bed60d8795556c157ae2f75095781b84
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-09-15 09:37:55 +0530

    ADD: About us

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
 import Home from "./pages/Home";
 import Login from './pages/Login';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './pages/Signup';
 import Contact from './pages/Contact';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
-    <>
+    <React.Fragment>
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/login" element={<Login />}/>
         <Route exact path="/signup" element={<Signup />}/>
         <Route exact path="/contact" element={<Contact />}/>
+        <Route path="*" element={<h1>404 Not Found</h1>}/>
+        <Route path='/about' element={<AboutUs/>}/>
+
       </Routes>
     </BrowserRouter>
       
-    </>
+    </React.Fragment>
   );
 }
 
